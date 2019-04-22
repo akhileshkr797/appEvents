e6c1c2429acbe17cd7114ad7a18fa5b09a26b47d
akhileshkr797 <akhileshkr797@gmail.com>
2019-04-22 18:35:41 +0530

    added appEvents

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const electron = require('electron')
-const { app, BrowserWindow, webContents, ipcMain } = electron
+const { app, BrowserWindow, webContents, ipcMain, Menu } = electron
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
 
 let mainWindow
+
+var force_quit = false
+let template = [{
+    label: 'appEvents',
+    submenu: [{
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        click: function() {
+            //force_quit = true
+            app.quit()
+        }
+    }]
+}]
 
 
 function createWindow() {
@@ -23,27 +36,50 @@ function createWindow() {
     }))
 
     mainWindow.once('ready-to-show', () => {
+        console.log('ready-to-show')
         mainWindow.show()
     })
 
-    mainWindow.on('close', function() {
+    mainWindow.on('close', function(e) {
+        console.log('close')
         mainWindow = null
     })
 
 }
 
-app.on('ready', () => {
-    createWindow()
-    console.log('ready')
+app.on('activate', function() {
+    console.log('activate')
+    if (mainWindow == null) {
+        createWindow()
+    }
+})
+
+//before-quit, will-quit, quit
+app.on('before-quit', function(e) {
+    console.log('before-quit')
+    force_quit = true
+})
+
+app.on('will-quit', function() {
+    console.log('will-quit')
+    mainWindow = null
 })
 
 app.on('window-all-closed', () => {
-    app.quit()
+
     console.log('window-all-closed')
+    if (process.platform != 'darwin') {
+        app.quit()
+    }
+
 })
 
-app.on('activate', (event, hasVisibleWindows) => {
-    if (!hasVisibleWindows) {
-        createWindow()
-    }
+
+
+
+app.on('ready', () => {
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+    createWindow()
+    console.log('ready')
 })
